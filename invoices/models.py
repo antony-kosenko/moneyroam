53496e59a1682a85167eef437d745f2c8084e3d9
antony-kosenko <kosenko.anton.work@gmail.com>
2023-08-06 14:19:56 +0300

    feature: invoice model and invoice category model created

---
+++ invoices/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+CURRENCY_CHOICE = (
+    ("₴", "hryvnia"),
+    ("$", "dollar"),
+    ("€", "euro")
+)
+
+class Category(MPTTModel):
+    """ Category model. 
+    Represents a category where money spent/earned"""
+
+    name = models.CharField(max_length=54, unique=True)
+    parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    slug = models.SlugField(unique=True, max_length=54, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ('name',)
+
+class Invoice(models.Model):
+    """ Invoice model.
+     Represents a basic income/outcome transaction. """
+    
+    title = models.CharField(max_length=64, verbose_name="Title")
+    slug = models.SlugField(unique=True, max_length=64, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    money_transaction = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Income/Outcome")
+    currency = models.CharField(max_length=1, choices=CURRENCY_CHOICE)
+    date_created = models.DateField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        ordering = ("-date_created",)
+        verbose_name_plural = "Invoices"
+
+    def __str__(self):
+        return f"[{self.data_created}] {self.title} [{self.money_transaction}]"
+        
+    def __repr__(self) -> str:
+        return f"{self.money_transaction} | {self.title}"
+        
--- moneyroam/settings.py
+++ moneyroam/settings.py
@@ -26,6 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party packages
+    "mptt",
+
+    # custom apps
+    "invoices",
 ]
 
 MIDDLEWARE = [
