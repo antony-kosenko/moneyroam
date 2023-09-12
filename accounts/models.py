1d4ae6b7dee827ac04facf530cce6e791847e5de
antony-kosenko <kosenko.anton.work@gmail.com>
2023-09-12 23:47:14 +0300

    feature: implemented user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,65 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from accounts.managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     """Custom User model extends a pre-defined django AbstractUser model"""
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=30, unique=True)
-    email = models.EmailField(unique=True, max_length=255)
-    first_name = models.CharField(max_length=150, null=True, blank=True)
-    last_name = models.CharField(max_length=150, null=True, blank=True)
+    email = models.EmailField(_("email address"), blank=True, unique=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True, blank=True, null=True)
 
     objects = CustomUserManager()
 
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    
     def __str__(self):
-        return self.username
+        return self.email
+
+
+class Profile(models.Model):
+    
+    """ Profile model. Contains user's extra data. """
+    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    avatar = models.ImageField(upload_to=f"uploads/{username}/profile_photos/", blank=True, null=True)
+    create_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_date = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
+    
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+    
