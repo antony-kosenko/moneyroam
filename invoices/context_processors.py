e6af6b230faacd966f5465030026598f0376854d
antony_kosenko <kosenko.anton.work@gmail.com>
2024-01-11 23:48:39 +0200

    transactions filters arranged and styled

--- invoices/context_processors.py
+++ invoices/context_processors.py
@@ -1,5 +1,7 @@
 from invoices.forms import NewInvoiceForm
 
+
 def create_transaction_form(request):
     """ Contains a form for new transaction creation """
-    return {"form": NewInvoiceForm()}
+    user_currency = request.user.config.currency
+    return {"form": NewInvoiceForm(initial={'currency': user_currency})}
