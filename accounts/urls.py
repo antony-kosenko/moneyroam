989b758ce28ea67203cbe7abac3bb1684b5b86dc
antony_kosenko <kosenko.anton.work@gmail.com>
2024-02-20 16:16:56 +0200

    login_required added to views ineed requires user to log in. Logout added. Overall login/registration views improved.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
-from accounts.views import registration_view, login_view
+from accounts.views import registration_view, login_view, logout_view
 
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", login_view, name="login"),
     path("registration/", registration_view, name="registration"),
+    path("login/", login_view, name="login"),
+    path("logout/", login_required(logout_view), name="logout")
 ]
--- invoices/urls.py
+++ invoices/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+
 from invoices.views import DashboardView, TransactionsListView, create_transaction_view
 
 app_name = "invoices"
 
 urlpatterns = [
-    path("dashboard/", DashboardView.as_view(), name="dashboard"),
-    path("transactions/", TransactionsListView.as_view(), name="transactions"),
-    path("transactions/create", create_transaction_view, name="create_transaction"),
+    path("dashboard/", login_required(DashboardView.as_view()), name="dashboard"),
+    path("transactions/", login_required(TransactionsListView.as_view()), name="transactions"),
+    path("transactions/create", login_required(create_transaction_view), name="create_transaction"),
 ]
