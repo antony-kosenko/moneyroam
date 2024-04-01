c8eb1a7fcf42a7ea1cf64134e5f784b58c4bb64c
antony_kosenko <kosenko.anton.work@gmail.com>
2024-04-01 15:04:21 +0300

    added script to reveal transaction comment on mouse hover

--- invoices/static/invoices/js/data_expand_on_hover.js
+++ invoices/static/invoices/js/data_expand_on_hover.js
@@ -1,2 +1,34 @@
-let commentItem = document.getElementById("comment-icon")
-let invoiceItem = document.getElementById("invoice-icon")
+const commentIcon = document.getElementById("commentIcon")
+const commentElement = document.getElementById("transactionComment").textContent
+const commentSection = document.getElementById("commentSection")
+
+var commentDiv = document.createElement("div")
+var commentContainer = document.createElement("span")
+
+commentIcon.addEventListener("mouseover", expandComment)
+commentIcon.addEventListener("mouseout", hideComment)
+
+function expandComment() {
+    // console.log(getMousePosition.posX)
+    commentContainer.textContent = commentElement
+    commentDiv.appendChild(commentContainer)
+    commentSection.appendChild(commentDiv)
+
+    if (commentDiv.classList.contains("hided_item")) {
+        commentDiv.classList.remove("hided_item")
+        commentDiv.classList.add("revealed_item")
+    }else{
+        commentDiv.classList.add("revealed_item")
+    }
+        
+}
+
+function hideComment() {    
+    if (commentDiv.classList.contains("revealed_item")) {
+        commentDiv.classList.remove("revealed_item")
+        commentDiv.classList.add("hided_item")
+    }else{
+        commentDiv.classList.add("hided_item")
+    }
+    commentDiv.removeChild(commentContainer)
+}
