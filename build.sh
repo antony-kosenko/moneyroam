e487e7bcd3e49b16b77ff0f49ddc7fa811aa4fdc
antony_kosenko <kosenko.anton.work@gmail.com>
2024-04-29 14:04:58 +0300

    build.sh appended with fixtures load

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# loading initial data to DB (fixteres)
+django-admin loaddata db_default_categories.json
