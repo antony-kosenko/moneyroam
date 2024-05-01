c95af75d52d38549af13f9ef93d2f35965664427
antony_kosenko <kosenko.anton.work@gmail.com>
2024-05-01 14:22:22 +0300

    debugging production

--- settings/base_settings.py
+++ settings/base_settings.py
@@ -233,3 +233,8 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
 	"default": dj_database_url.parse(os.getenv("DB_URL"))
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
